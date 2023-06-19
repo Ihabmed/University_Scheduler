648e2463ab6483de9c31af47a8784ed9f13729ca
issamHosni <issam.hosni@univ-tiaret.dz>
2023-06-19 15:20:07 +0100

    done 4

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import re
 
 
 def organizer(path):
@@ -12,13 +13,16 @@ def organizer(path):
     groupes = emp["Groupe"].unique()
 
     groupes = groupes.tolist()
-    groupes.remove("Promo")
     
-
-    groupesEmp = []
+    regex = re.compile(r"[a-zA-Z]+")
 
     for groupe in groupes:
-        df = emp.loc[(emp['Groupe'].str.contains(groupe)) | ((emp['Groupe'] == "Promo") | (emp['Groupe'] == "Sec"))]
+        if(regex.match(groupe)):
+            groupes.remove(groupe)
+    print(groupes)
+
+    for groupe in groupes:
+        df = emp.loc[(emp['Groupe'].str.contains(groupe)) | (emp['Groupe'] == "Promo") | (emp['Groupe'] == "Sec") | (emp['Groupe'] == "sec")]
         fig, ax =plt.subplots(figsize=(12,4))
         ax.axis('tight')
         ax.axis('off')
@@ -27,10 +31,9 @@ def organizer(path):
         pp.savefig(fig, bbox_inches='tight')
         pp.close()
 
-    print(groupesEmp)
 
 
-organizer('Emplois du temps.csv')
+organizer('Emp1erS2.csv')
 
 
 
