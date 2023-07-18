b22b4afb4f4a4f8fd1ba71a48252bc7a68e3e850
Ihabmed <94643335+Ihabmed@users.noreply.github.com>
2023-07-18 13:00:36 +0100

    fixed specialities

--- code_1.py
+++ code_1.py
@@ -4,14 +4,12 @@ from matplotlib.backends.backend_pdf import PdfPages
 import tabula
 import os
 
+# emp.columns = ["Journée", "Heure", "Module", "Type", 
+    # "Enseignant", "Salle", "Promotion", "Spésialité", "Groupe", "Nature"]
 def has_numbers(inputString):
     return any(char.isdigit() for char in inputString)
 
-def organizer(path):
-    tabula.convert_into(path, "output.csv", output_format="csv", pages='all')
-    # emp.columns = ["Journée", "Heure", "Module", "Type", 
-    # "Enseignant", "Salle", "Promotion", "Spésialité", "Groupe", "Nature"]
-    emp = pd.read_csv("output.csv")
+def group_orginizer(path, emp):
     groupes = emp["Groupe"].unique()
     groupes = groupes.tolist()
     for i in range(len(groupes)):
@@ -27,10 +25,6 @@ def organizer(path):
             if ((groupes[i] + "+") in groupes[j]) or (("+" + groupes[i]) in groupes[j]) or (not has_numbers(groupes[j])):
                 tmp.append(groupes[j])
         grp.append(tmp)
-    if not os.path.isdir(path.split(".")[0]):
-        pwd = os.getcwd()
-        dirpath = os.path.join(pwd, path.split(".")[0])
-        os.mkdir(dirpath)
 
     for groupe in grp:
         df = emp.loc[(emp['Groupe'].isin(groupe))]
@@ -38,33 +32,42 @@ def organizer(path):
         ax.axis('tight')
         ax.axis('off')
         the_table = ax.table(cellText=df.values,colLabels=df.columns,loc='center')
-        pp = PdfPages(f"/home/ihab/Downloads/groupe{groupe[0]}.pdf")
+        pp = PdfPages(f"{path}/groupe{groupe[0]}.pdf")
         pp.savefig(fig, bbox_inches='tight')
         pp.close()
 
-    os.remove('output.csv')
+def organizer(path):
+    tabula.convert_into(path, os.path.dirname(path) + "/" + "output.csv", output_format="csv", pages='all')
+    emp = pd.read_csv(os.path.dirname(path) + "/" + "output.csv")
+    speciality = emp["Spésialité"].unique()
+    speciality = speciality.tolist()
+    if len(speciality) > 1:
+        spec = []
+        for i in range(len(speciality)):
+            if "+" in speciality[i]:
+                continue
+            spec1 = []
+            spec1.append(speciality[i])
+            for j in range(len(speciality)):
+                if ((speciality[i] + "+") in speciality[j]) or (("+" + speciality[i]) in speciality[j]):
+                    spec1.append(speciality[j])
+            spec.append(spec1)
+        for s in spec:
+            df = emp.loc[emp["Spésialité"].isin(s)]
+            p = os.path.join(os.path.dirname(path), s[0])
+            if not os.path.exists(p):
+                os.mkdir(p)
+            else:
+                for dir in os.listdir(p):
+                    os.remove(p + "/" + dir)
+            df.to_csv(f"{p}/output1.csv", sep=",",index=False, encoding='utf-8')
+            group_orginizer(p, pd.read_csv(f"{p}/output1.csv"))
+            os.remove(f"{p}/output1.csv")
+    else:
+        os.mkdir(path.split(".")[0])
+        group_orginizer(path.split(".")[0], emp)
+    os.remove(os.path.dirname(path) + "/" + "output.csv")
 
 
 
-organizer('/home/ihab/Downloads/L3.pdf')
-
-
-
-
-
-# speciality = emp["Spésialité"].unique()
-# speciality = speciality.tolist()
-# spec = []
-# for i in range(len(speciality)):
-#     if "+" in speciality[i]:
-#         continue
-#     spec1 = []
-#     spec1.append(speciality[i])
-#     for j in range(len(speciality)):
-#         if ((speciality[i] + "+") in speciality[j]) or (("+" + speciality[i]) in speciality[j]):
-#             spec1.append(speciality[j])
-#     spec.append(spec1)
-
-# for s in spec:
-#     df = emp.loc[emp["Spésialité"].isin(s)]
-#     print(df)
+organizer('/home/ihab/Downloads/Master1.pdf')
